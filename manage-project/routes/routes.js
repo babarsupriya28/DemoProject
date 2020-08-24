ac87768d6031331495d4112b3220ca59727011e2
babarsupriya28 <babarsupriya28@gmail.com>
2020-08-24 18:33:55 +0530

    install nodemon,express,bodyparsle,create  proj in angular

---
+++ manage-project/routes/routes.js
@@ -0,0 +1,11 @@
+// routes.js
+
+const express = require("express");
+
+// express.Router() : Routing refers to how an application's endpoints (URIs) respond to client requests.
+// You define routing using methods of the Express app object that correspond to HTTP methods.
+
+const router = express.Router();
+
+
+module.exports = router;
